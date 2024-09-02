7153394fd8cd4456fe3cf56b600b4c69476f8bdc
Souvanxay LUANGSOUVANNAVONG <souvanxay38@gmail.com>
2024-09-02 22:22:45 +0200

    Add Stripe Plugin

--- WebSharper.Capacitor.Community/Main.fs
+++ WebSharper.Capacitor.Community/Main.fs
@@ -6,6 +6,12 @@ open WebSharper.InterfaceGenerator
 
 module Definition =
     [<AutoOpen>]
+    module Core = 
+        let PluginListenerHandle =
+            Class "PluginListenerHandle"
+            |+> Instance ["remove" => T<unit> ^-> T<Promise<unit>>]
+
+    [<AutoOpen>]
     module FacebookLogin = 
         let AccessToken =
             Pattern.Config "AccessToken" {
@@ -82,20 +88,357 @@ module Definition =
                 "setAdvertiserIDCollectionEnabled" => T<bool>?options ^-> T<Promise<unit>>
             ]
 
+    [<AutoOpen>]
+    module Stripe = 
+        let ApplePayEventsEnum =
+            Pattern.EnumInlines "ApplePayEventsEnum" [
+                "Loaded", "applePayLoaded"
+                "FailedToLoad", "applePayFailedToLoad"
+                "Completed", "applePayCompleted"
+                "Canceled", "applePayCanceled"
+                "Failed", "applePayFailed"
+                "DidSelectShippingContact", "applePayDidSelectShippingContact"
+                "DidCreatePaymentMethod", "applePayDidCreatePaymentMethod"
+            ]
+
+        let GooglePayEventsEnum =
+            Pattern.EnumInlines "GooglePayEventsEnum" [
+                "Loaded", "googlePayLoaded"
+                "FailedToLoad", "googlePayFailedToLoad"
+                "Completed", "googlePayCompleted"
+                "Canceled", "googlePayCanceled"
+                "Failed", "googlePayFailed"
+            ]
+
+        let PaymentFlowEventsEnum =
+            Pattern.EnumInlines "PaymentFlowEventsEnum" [
+                "Loaded", "paymentFlowLoaded"
+                "FailedToLoad", "paymentFlowFailedToLoad"
+                "Opened", "paymentFlowOpened"
+                "Created", "paymentFlowCreated"
+                "Completed", "paymentFlowCompleted"
+                "Canceled", "paymentFlowCanceled"
+                "Failed", "paymentFlowFailed"
+            ]
+
+        let PaymentSheetEventsEnum =
+            Pattern.EnumInlines "PaymentSheetEventsEnum" [
+                "Loaded", "paymentSheetLoaded"
+                "FailedToLoad", "paymentSheetFailedToLoad"
+                "Completed", "paymentSheetCompleted"
+                "Canceled", "paymentSheetCanceled"
+                "Failed", "paymentSheetFailed"
+            ]
+
+        let ApplePayResultInterface = 
+            Pattern.EnumInlines "ApplePayResultInterface" [
+                "Completed", "applePayCompleted"
+                "Canceled", "applePayCanceled"
+                "Failed", "applePayFailed"
+                "DidSelectShippingContact", "applePayDidSelectShippingContact"
+                "DidCreatePaymentMethod", "applePayDidCreatePaymentMethod"
+            ]
+
+        let GooglePayResultInterface = 
+            Pattern.EnumInlines "GooglePayResultInterface" [
+                "Completed", "googlePayCompleted"
+                "Canceled", "googlePayCanceled"
+                "Failed", "googlePayFailed"
+            ]
+
+        let CollectionMode = 
+            Pattern.EnumStrings "CollectionMode" [
+                "automatic"
+                "always"
+            ]
+
+        let AddressCollectionMode = 
+            Pattern.EnumStrings "AddressCollectionMode" [
+                "automatic"
+                "full"
+            ]
+
+        let PaymentFlowResultInterface = 
+            Pattern.EnumInlines "PaymentFlowResultInterface" [
+                "Completed", "paymentFlowCompleted"
+                "Canceled", "paymentFlowCanceled"
+                "Failed", "paymentFlowFailed"
+            ]
+
+        let PaymentSheetResultInterface = 
+            Pattern.EnumInlines "PaymentSheetResultInterface" [
+                "Completed", "paymentSheetCompleted"
+                "Canceled", "paymentSheetCanceled"
+                "Failed", "paymentSheetFailed"
+            ]
+
+        let PaymentSummaryType = 
+            Pattern.Config "PaymentSummaryType" {
+                Required = []
+                Optional = [
+                    "label", T<string>
+                    "amount", T<int>
+                ]
+            }
+
+        let ShippingContactType = 
+            Pattern.EnumStrings "ShippingContactType" [
+                "postalAddress"
+                "phoneNumber"
+                "emailAddress"
+                "name"
+            ]
+
+        let CreateApplePayOption = 
+            Pattern.Config "CreateApplePayOption" {
+                Required = []
+                Optional = [
+                    "paymentIntentClientSecret", T<string>
+                    "paymentSummaryItems", !| PaymentSummaryType
+                    "merchantIdentifier", T<string>
+                    "countryCode", T<string>
+                    "currency", T<string>
+                    "requiredShippingContactFields", !| ShippingContactType
+                    "allowedCountries", !| T<string>
+                    "allowedCountriesErrorDescription", T<string>
+                ]
+            }
+
+        let ShippingContact = 
+            Pattern.Config "ShippingContact" {
+                Required = []
+                Optional = [
+                    "givenName", T<string>
+                    "familyName", T<string>
+                    "middleName", T<string>
+                    "namePrefix", T<string>
+                    "nameSuffix", T<string>
+                    "nameFormatted", T<string>
+                    "phoneNumber", T<string>
+                    "nickname", T<string>
+                    "street", T<string>
+                    "city", T<string>
+                    "state", T<string>
+                    "postalCode", T<string>
+                    "country", T<string>
+                    "isoCountryCode", T<string>
+                    "subAdministrativeArea", T<string>
+                    "subLocality", T<string>
+                ]
+            }
+
+        let DidSelectShippingContact = 
+            Pattern.Config "DidSelectShippingContact" {
+                Required = []
+                Optional = [
+                    "contact", ShippingContact.Type
+                ]
+            }
+
+        let CreateGooglePayOption = 
+            Pattern.Config "CreateGooglePayOption" {
+                Required = []
+                Optional = [
+                    "paymentIntentClientSecret", T<string>
+                    "paymentSummaryItems", PaymentSummaryType.Type
+                    "merchantIdentifier", T<string>
+                    "countryCode", T<string>
+                    "currency", T<string>
+                ]
+            }
+
+        let StyleType = 
+            Pattern.EnumStrings "StyleType" [
+                "alwaysLight"
+                "alwaysDark"
+            ]
+
+        let BillingDetailsCollectionConfiguration = 
+            Pattern.Config "BillingDetailsCollectionConfiguration" {
+                Required = []
+                Optional = [
+                    "email", CollectionMode.Type
+                    "name", CollectionMode.Type
+                    "phone", CollectionMode.Type
+                    "address", AddressCollectionMode.Type
+                ]
+            }
+
+        let CreatePaymentFlowOption = 
+            Pattern.Config "CreatePaymentFlowOption" {
+                Required = []
+                Optional = [
+                    "paymentIntentClientSecret", T<string>
+                    "setupIntentClientSecret", T<string>
+                    "billingDetailsCollectionConfiguration", BillingDetailsCollectionConfiguration.Type
+                    "customerEphemeralKeySecret", T<string>
+                    "customerId", T<string>
+                    "enableApplePay", T<bool>
+                    "applePayMerchantId", T<string>
+                    "enableGooglePay", T<bool>
+                    "GooglePayIsTesting", T<bool>
+                    "countryCode", T<string>
+                    "merchantDisplayName", T<string>
+                    "returnURL", T<string>
+                    "style", StyleType.Type
+                    "withZipCode", T<bool>
+                ]
+            }
+
+        let CreatePaymentSheetOption = 
+            Pattern.Config "CreatePaymentSheetOption" {
+                Required = []
+                Optional = [
+                    "paymentIntentClientSecret", T<string>
+                    "setupIntentClientSecret", T<string>
+                    "billingDetailsCollectionConfiguration", BillingDetailsCollectionConfiguration.Type
+                    "customerEphemeralKeySecret", T<string>
+                    "customerId", T<string>
+                    "enableApplePay", T<bool>
+                    "applePayMerchantId", T<string>
+                    "enableGooglePay", T<bool>
+                    "GooglePayIsTesting", T<bool>
+                    "countryCode", T<string>
+                    "merchantDisplayName", T<string>
+                    "returnURL", T<string>
+                    "style", StyleType.Type
+                    "withZipCode", T<bool>
+                ]
+            }
+
+        let StripeInitializationOptions = 
+            Pattern.Config "StripeInitializationOptions" {
+                Required = []
+                Optional = [
+                    "publishableKey", T<string>
+                    "stripeAccount", T<string>
+                ]
+            }
+
+        let StripeURLHandlingOptions = 
+            Pattern.Config "StripeURLHandlingOptions" {
+                Required = []
+                Optional = [
+                    "url", T<string>
+                ]
+            }
+
+        let PresentApplePayResult = 
+            Pattern.Config "PresentApplePayResult" {
+                    Required = [
+                        "paymentResult", ApplePayResultInterface.Type
+                    ]
+                    Optional = []
+                }
+
+        let PresentGooglePayResult = 
+            Pattern.Config "PresentGooglePayResult" {
+                Required = [
+                    "paymentResult", GooglePayResultInterface.Type
+                ]
+                Optional = []
+            }
+
+        let PresentPaymentFlowResult = 
+            Pattern.Config "PresentPaymentFlowResult" {
+                Required = [
+                    "cardNumber", T<string>
+                ]
+                Optional = []
+            }
+
+        let ConfirmPaymentFlowResult = 
+            Pattern.Config "ConfirmPaymentFlowResult" {
+                Required = [
+                    "paymentResult", PaymentFlowResultInterface.Type
+                ]
+                Optional = []
+        }
+
+        let PresentPaymentSheetResult = 
+            Pattern.Config "PresentPaymentSheetResult" {
+                Required = [
+                    "paymentResult", PaymentSheetResultInterface.Type
+                ]
+                Optional = []
+            }
+
+        let StripePlugin = 
+            Class "StripePlugin" 
+            |+> Instance [
+                "initialize" => StripeInitializationOptions?opts ^-> T<Promise<unit>>
+                "handleURLCallback" => StripeURLHandlingOptions?opts ^-> T<Promise<unit>>
+                "isApplePayAvailable" => T<unit> ^-> T<Promise<unit>>
+                "createApplePay" => CreateApplePayOption?options ^-> T<Promise<unit>>
+                "presentApplePay" => T<unit> ^-> T<Promise<_>>[PresentApplePayResult]
+                "addListener" => ApplePayEventsEnum?eventName * (T<unit> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'applePayLoaded', 'applePayCompleted' and 'applePayCanceled' events"
+                "addListener" => ApplePayEventsEnum?eventName * (T<string> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'applePayFailedToLoad' and 'applePayFailed' events"
+                "addListener" => ApplePayEventsEnum?eventName * (DidSelectShippingContact ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'applePayDidSelectShippingContact' and 'applePayDidCreatePaymentMethod' events"
+
+                "isGooglePayAvailable" => T<unit> ^-> T<Promise<unit>>
+                "createGooglePay" => CreateGooglePayOption?options ^-> T<Promise<unit>>
+                "presentGooglePay" => T<unit> ^-> T<Promise<_>>[PresentGooglePayResult]
+                "addListener" => GooglePayEventsEnum?eventName * (T<unit> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'googlePayLoaded', 'googlePayCompleted' and 'googlePayCanceled' events"
+                "addListener" => GooglePayEventsEnum?eventName * (T<string> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'googlePayFailedToLoad' and 'googlePayFailed' events"
+
+                "createPaymentFlow" => CreatePaymentFlowOption?options ^-> T<Promise<unit>>
+                "presentPaymentFlow" => T<unit> ^-> T<Promise<_>>[PresentPaymentFlowResult]
+                "confirmPaymentFlow" => T<unit> ^-> T<Promise<_>>[ConfirmPaymentFlowResult]
+                "addListener" => PaymentFlowEventsEnum?eventName * (T<unit> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'paymentFlowLoaded', 'paymentFlowOpened', 'paymentFlowCompleted', and 'paymentFlowCanceled' events"
+                "addListener" => PaymentFlowEventsEnum?eventName * (T<string> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'paymentFlowFailedToLoad' and 'paymentFlowFailed' events"
+                "addListener" => PaymentFlowEventsEnum?eventName * (PresentPaymentFlowResult ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'paymentFlowCreated' events"
+
+                "createPaymentSheet" => CreatePaymentSheetOption?options ^-> T<Promise<unit>>
+                "presentPaymentSheet" => T<unit> ^-> T<Promise<_>>[PresentPaymentSheetResult]
+                "addListener" => PaymentSheetEventsEnum?eventName * (T<unit> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'paymentSheetLoaded', 'paymentSheetCompleted', and 'paymentSheetCanceled' events"
+                "addListener" => PaymentSheetEventsEnum?eventName * (T<string> ^-> T<unit>)?listenerFunc ^-> T<Promise<_>>[PluginListenerHandle]
+                |> WithComment "Listen 'paymentSheetFailedToLoad' and 'paymentSheetFailed' events"
+            ]
+
+        let CapacitorStripeContext = 
+            Pattern.Config "CapacitorStripeContext" {
+                Required = []
+                Optional = [
+                    "stripe", StripePlugin.Type
+                    "isApplePayAvailable", T<bool>
+                    "isGooglePayAvailable", T<bool>
+                ]
+            }
+
     let CapacitorCommunity = 
         Class "Capacitor.Community"
         |+> Static [
             "FacebookLogin" =? FacebookLoginPlugin
             |> Import "FacebookLogin" "@capacitor-community/facebook-login"
+            "Stripe" =? StripePlugin
+            |> Import "Stripe" "@capacitor-community/stripe"
         ]
 
     let Assembly =
         Assembly [
             Namespace "Websharper.Capacitor.Community" [
+                PluginListenerHandle
                 FacebookLoginPlugin
+                StripePlugin
             ]
             Namespace "Websharper.Capacitor.Community.FacebookLogin" [
                 FacebookConfiguration; LoginOptions; FacebookLoginResponse; FacebookCurrentAccessTokenResponse; ProfileOptions; AccessToken
+            ]
+            Namespace "Websharper.Capacitor.Community.Stripe" [
+                CapacitorStripeContext; PresentPaymentSheetResult; ConfirmPaymentFlowResult; PresentPaymentFlowResult; PresentGooglePayResult
+                StripeURLHandlingOptions; StripeInitializationOptions; CreatePaymentSheetOption; CreatePaymentFlowOption; BillingDetailsCollectionConfiguration
+                CreateGooglePayOption; DidSelectShippingContact; ShippingContact; CreateApplePayOption; ShippingContactType; PaymentSummaryType
+                PaymentFlowResultInterface; AddressCollectionMode; CollectionMode; GooglePayResultInterface; ApplePayResultInterface; PaymentSheetEventsEnum
+                GooglePayEventsEnum; ApplePayEventsEnum; PresentApplePayResult; StyleType; PaymentSheetResultInterface; PaymentFlowEventsEnum
             ]
         ]
 
